e778d411d4879adbdc44e1d4e21c90ddd2d2d577
Alex Kotliarskyi <alex.frantic@gmail.com>
2022-08-08 19:00:52 -0700

    Prettier

--- scripts/make.js
+++ scripts/make.js
@@ -1,65 +1,67 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
-const yaml = require('./lib/js-yaml.min');
-const hljs = require('./lib/highlight.min');
-const MarkdownIt = require('./lib/markdown-it.min');
-const {Liquid} = require('./lib/liquid.browser.umd');
+const fs = require("fs");
+const path = require("path");
+const yaml = require("./lib/js-yaml.min");
+const hljs = require("./lib/highlight.min");
+const MarkdownIt = require("./lib/markdown-it.min");
+const { Liquid } = require("./lib/liquid.browser.umd");
 
 const md = new MarkdownIt({
   html: true,
   typographer: true,
   xhtmlOut: true,
   highlight: (code, language) => {
-   if (language && hljs.getLanguage(language)) {
-     try {
-       return hljs.highlight(code, {language}).value;
-     } catch (__) {}
-   }
-   return '';
+    if (language && hljs.getLanguage(language)) {
+      try {
+        return hljs.highlight(code, { language }).value;
+      } catch (__) {}
+    }
+    return "";
   },
 });
 
 const liquid = new Liquid({
   fs: {
-    readFileSync: (file) => fs.readFileSync(file, 'utf8'),
+    readFileSync: (file) => fs.readFileSync(file, "utf8"),
     existsSync: (file) => fs.existsSync(file),
     resolve: (root, file, ext) => path.resolve(root, ext ? file + ext : file),
   },
-  root: path.resolve(__dirname, '..', '_includes/'),
+  root: path.resolve(__dirname, "..", "_includes/"),
   dynamicPartials: false,
 });
 
-liquid.registerFilter('group_by_year', (posts) => { 
+liquid.registerFilter("group_by_year", (posts) => {
   const items = new Map();
   for (const post of posts) {
-    const date = post.date.split('-')[0];
+    const date = post.date.split("-")[0];
     if (!items.has(date)) {
       items.set(date, []);
     }
     items.get(date).push(post);
   }
 
-  return [...items.entries()].map(([name, items]) => ({name, items}));
-})
+  return [...items.entries()].map(([name, items]) => ({ name, items }));
+});
 
 function inferFromFileName(fileName) {
-  const basename = path.basename(fileName).split('.')[0];
+  const basename = path.basename(fileName).split(".")[0];
   const match = basename.match(/^(\d\d\d\d-\d\d-\d\d)-(.*)$/);
   if (!match) {
     return { url: basename };
   }
   return {
-    url:  '/' + match[2],
-    date: match[1] + 'T12:00:00+00:00',
+    url: "/" + match[2],
+    date: match[1] + "T12:00:00+00:00",
   };
 }
 
 function load(fileName) {
-  const content = fs.readFileSync(fileName, 'utf8');
-  const format = fileName.endsWith('.md') ? (content) => md.render(content) : (content) => content;
-  if (content.startsWith('---\n')) {
+  const content = fs.readFileSync(fileName, "utf8");
+  const format = fileName.endsWith(".md")
+    ? (content) => md.render(content)
+    : (content) => content;
+  if (content.startsWith("---\n")) {
     const [_, rawHeader, rawBody] = content.split(/^---$/gm);
     return {
       header: {
@@ -67,56 +69,65 @@ function load(fileName) {
         ...yaml.load(rawHeader),
       },
       body: format(rawBody.trim()),
-    }
+    };
   } else {
     return {
       header: {},
       body: format(content),
-    }
+    };
   }
 }
 
 function render(fileName, data = {}) {
-  let {header: {layout, ...page}, body} = load(fileName);
-  
-  data = {...data, page: {...page, ...data.page}};
+  let {
+    header: { layout, ...page },
+    body,
+  } = load(fileName);
+
+  data = { ...data, page: { ...page, ...data.page } };
   body = liquid.parseAndRenderSync(body, data);
 
   if (layout) {
-    return render(path.join(__dirname, '..', '_layouts', layout + '.html'), {...data, content: body});
+    return render(path.join(__dirname, "..", "_layouts", layout + ".html"), {
+      ...data,
+      content: body,
+    });
   }
 
   return body;
 }
 
-const site = yaml.load(fs.readFileSync('_config.yml', 'utf8'));
+const site = yaml.load(fs.readFileSync("_config.yml", "utf8"));
 site.time = new Date().toISOString();
 
-site.posts = fs.readdirSync('_posts')
-  .map(file => {
-    const {header, body} = load(path.join('_posts', file));
-    return {...header, content: body};
+site.posts = fs
+  .readdirSync("_posts")
+  .map((file) => {
+    const { header, body } = load(path.join("_posts", file));
+    return { ...header, content: body };
   })
   .sort((p1, p2) => p2.date.localeCompare(p1.date));
 
 function renderDir(dir) {
   for (const file of fs.readdirSync(dir)) {
-    if (!['.html', '.md', '.txt', '.xml'].includes(path.extname(file))) {
+    if (![".html", ".md", ".txt", ".xml"].includes(path.extname(file))) {
       continue;
     }
 
     const source = path.join(dir, file);
     process.stdout.write(`Building ${source}\n`);
-    const {header: {url}} = load(source);
+    const {
+      header: { url },
+    } = load(source);
 
-    const destination = ['.md', '.html'].includes(path.extname(file)) 
-      ? path.join('_build', url, 'index.html')
-      : path.join('_build', file);
+    const destination = [".md", ".html"].includes(path.extname(file))
+      ? path.join("_build", url, "index.html")
+      : path.join("_build", file);
 
-    fs.mkdirSync(path.dirname(destination), {recursive: true});
-    fs.writeFileSync(destination, render(path.join(dir, file), {site}));
+    fs.mkdirSync(path.dirname(destination), { recursive: true });
+    fs.writeFileSync(destination, render(path.join(dir, file), { site }));
   }
 }
 
-renderDir('_posts');
-renderDir('pages');
+renderDir("_posts");
+renderDir("pages");
